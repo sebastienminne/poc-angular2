15e2e95cf9cc6ff63848398189ac7ce3f617feb5
DOMAIN\sminne <minne.sebastien@gmail.com>
2016-09-12 16:55:48 +0200

    init back end project

---
+++ back-demo-primeng/src/main/java/com/seb/BackDemoPrimengApplication.java
@@ -0,0 +1,12 @@
+package com.seb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDemoPrimengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDemoPrimengApplication.class, args);
+	}
+}
